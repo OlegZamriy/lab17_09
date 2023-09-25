40bc8a38f8e28d1ce3e591c331d4755ee78326f2
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 22:17:48 +0300

    1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ number1 = float(input("Введіть перше число: "))
 
 number2 = float(input("Введіть друге число: "))
 
-if number1 > number2:
-    max_number = number1
+if number1 < number2:
+    min_number = number1
 else:
-    max_number = number2
+    min_number = number2
 
-print(f"Максимальне число: {max_number}")
+print(f"Мінімальне число: {min_number}")
