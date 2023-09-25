483bef0eb5168843167862ee322a96c73c6e955f
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 22:15:02 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 number = int(input("Введіть число: "))
 
-if number % 2 == 0:
-    print(f"{number} - Even number")
+if number % 7 == 0:
+    print(f"{number} - Number is a multiple of 7")
 else:
-    print(f"{number} - Odd number")
+    print(f"{number} - Number is not a multiple of 7")
